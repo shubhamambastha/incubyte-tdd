a4a4ba9eb51f954cdb0726d432d507ba6715ec51
Shubham Ambastha <shubhams.saurav@gmail.com>
2024-10-04 12:00:09 +0530

    using multiple helper functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,26 @@
+function escapeRegex(delimiter) {
+  return delimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+}
+
+function parseCustomDelimiter(numbers) {
+  const parts = numbers.split("\n");
+  let delimiter = parts[0].slice(2);
+  delimiter = escapeRegex(delimiter);
+  return { delimiter, numbers: parts[1] };
+}
+
+function validateAndSum(numbersArray) {
+  const negatives = numbersArray.filter((num) => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+  }
+  return numbersArray.reduce((sum, num) => sum + num, 0);
+}
+
+function splitNumbers(numbers, delimiter) {
+  return numbers.split(new RegExp(delimiter));
+}
+
 function add(numbers) {
   if (numbers === "") {
     return 0;
@@ -6,22 +29,15 @@ function add(numbers) {
   let delimiter = /,|\n/;
 
   if (numbers.startsWith("//")) {
-    const parts = numbers.split("\n");
-    let customDelimiter = parts[0].slice(2);
-    customDelimiter = customDelimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-    delimiter = new RegExp(customDelimiter);
-    numbers = parts[1];
+    const result = parseCustomDelimiter(numbers);
+    delimiter = result.delimiter;
+    numbers = result.numbers;
   }
 
-  const numArray = numbers.split(delimiter);
-  const negativeNumbers = numArray.filter((num) => num < 0);
-  if (negativeNumbers.length > 0) {
-    throw new Error(
-      `negative numbers not allowed: ${negativeNumbers.join(",")}`
-    );
-  }
-
-  return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
+  const numbersArray = splitNumbers(numbers, delimiter).map((num) =>
+    parseInt(num, 10)
+  );
+  return validateAndSum(numbersArray);
 }
 
 module.exports = { add };
