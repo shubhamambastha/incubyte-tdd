42bd61687673b443fc4de971eeea1f4266a68dd4
Shubham Ambastha <shubhams.saurav@gmail.com>
2024-10-04 11:41:58 +0530

    function to handle custom delimiter

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,16 @@ function add(numbers) {
   if (numbers === "") {
     return 0;
   }
-  const numArray = numbers.replace(/\n/g, ",").split(",");
+
+  let delimiter = /,|\n/;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0][2]);
+    numbers = parts[1];
+  }
+
+  const numArray = numbers.split(delimiter);
 
   return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
