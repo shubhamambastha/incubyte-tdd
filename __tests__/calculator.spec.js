ada1b08250dc6fb3429862cd83ac8196105ed9bd
Shubham Ambastha <shubhams.saurav@gmail.com>
2024-10-04 11:17:44 +0530

    test case for multiple numbers

--- __tests__/calculator.spec.js
+++ __tests__/calculator.spec.js
@@ -11,3 +11,7 @@ test("should return the number itself for a single number", () => {
 test("should return the sum of two comma-separated numbers", () => {
   expect(calculator.add("1,2")).toBe(3);
 });
+
+test("should return the sum of any number of comma-separated numbers", () => {
+  expect(calculator.add("1,2,3,4")).toBe(10);
+});
