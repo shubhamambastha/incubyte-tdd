c6b79b8117066999ac7ee38d381219a39ffdbbcd
Shubham Ambastha <shubhams.saurav@gmail.com>
2024-10-04 11:15:42 +0530

    added test for single number

--- __tests__/calculator.spec.js
+++ __tests__/calculator.spec.js
@@ -3,3 +3,7 @@ const calculator = require("../src/calculator");
 test("should return 0 for an empty string", () => {
   expect(calculator.add("")).toBe(0);
 });
+
+test("should return the number itself for a single number", () => {
+  expect(calculator.add("1")).toBe(1);
+});
